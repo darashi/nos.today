5313fd3b737e9546c31604cb0e3de2f71838810c
Yoji Shidara <dara@shidara.net>
2024-08-05 14:56:08 +0900

    WIP

--- src/app/search/Search.tsx
+++ src/app/search/Search.tsx
@@ -2,150 +2,157 @@
 
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import QueryForm from "../QueryForm";
-import { Mux, Event, RelayMessageEvent, EventMessage } from "nostr-mux";
 import { useApp } from "@/lib/App";
 import { Note } from "./Note";
 import { useSearchParams } from "next/navigation";
+import {
+	batch,
+	createRxBackwardReq,
+	createRxForwardReq,
+	EventPacket,
+	latestEach,
+	uniq,
+} from "rx-nostr";
+import { bufferTime } from "rxjs/operators";
+import type { Subscription as RxSubscription } from "rxjs";
+import type { Event, Content } from "nostr-typedef";
+import { regularRelays, searchRelays } from "@/lib/config";
 
 const purgeOldNotes = (notes: Record<string, Event>, limit: number) => {
-  const sortedNotes = Object.values(notes).sort(
-    (a: Event, b: Event) => a.created_at - b.created_at
-  );
+	const sortedNotes = Object.values(notes).sort(
+		(a: Event, b: Event) => a.created_at - b.created_at,
+	);
 
-  for (let i = 0; i < sortedNotes.length - limit; i++) {
-    delete notes[sortedNotes[i].id];
-  }
+	for (let i = 0; i < sortedNotes.length - limit; i++) {
+		delete notes[sortedNotes[i].id];
+	}
 };
 
 function normalize(str: string): string {
-  return str.normalize("NFKC").toLowerCase();
+	return str.normalize("NFKC").toLowerCase();
 }
 
 export default function Search() {
-  const app = useApp();
-  const params = useSearchParams();
-  const query = params.get("q") || "";
-  const queryTerms = useMemo(() => normalize(query).split(/\s+/), [query]);
-  const [possiblyMoreAvailable, setPossiblyMoreAvailable] = useState(false);
+	const app = useApp();
+	const params = useSearchParams();
+	const query = params.get("q") || "";
+	const queryTerms = useMemo(() => normalize(query).split(/\s+/), [query]);
+	const [possiblyMoreAvailable, setPossiblyMoreAvailable] = useState(false);
 
-  const [notes, setNotes] = useState<Record<string, Event>>({});
+	const [notes, setNotes] = useState<Record<string, Event>>({});
+	const [profiles, setProfiles] = useState<Record<string, Content.Metadata>>(
+		{},
+	);
 
-  const subscriptionRef = useRef("");
-  const limit = 100;
-  const hardLimit = 5000;
+	const rxSubscriptionRef = useRef<RxSubscription>();
 
-  const addNotes = useCallback(
-    (items: RelayMessageEvent<EventMessage>[]) => {
-      if (items.length > 0) {
-        setPossiblyMoreAvailable(true);
-      }
+	const limit = 100;
+	const hardLimit = 5000;
 
-      setNotes((prev) => {
-        let updated = { ...prev };
-        for (const item of items) {
-          const event = item.received.event;
-          // Relays may return events that do not actually match the query, so verify here.
-          const normalizedContent = normalize(event.content);
-          if (!queryTerms.every((term) => normalizedContent.includes(term))) {
-            continue;
-          }
-          updated[event.id] = event;
-        }
-        purgeOldNotes(updated, hardLimit);
-        return updated;
-      });
-    },
-    [queryTerms]
-  );
+	const addEvent = useCallback(
+		(event: Event) => {
+			setNotes((prev) => {
+				const updated = { ...prev };
+				// Relays may return events that do not actually match the query, so verify here.
+				const normalizedContent = normalize(event.content);
+				if (queryTerms.every((term) => normalizedContent.includes(term))) {
+					updated[event.id] = event;
+				}
+				purgeOldNotes(updated, hardLimit);
+				return updated;
+			});
+		},
+		[queryTerms],
+	);
 
-  const subscribe = useCallback(
-    (mux: Mux, search: string) => {
-      return mux.subscribe({
-        filters: [{ kinds: [1], search, limit } as any],
+	useEffect(() => {
+		setNotes({});
 
-        onEvent: addNotes,
+		if (query && app.rxNostr) {
+			const rxReq = createRxForwardReq("search");
+			const profileReq = createRxBackwardReq();
 
-        onRecovered: (relay) => {
-          console.log(
-            `relay(${relay.url}) was added or recovered. It joins subscription`
-          );
+			rxSubscriptionRef.current = app.rxNostr
+				.use(rxReq, { relays: searchRelays })
+				.pipe(uniq())
+				.subscribe((packet: EventPacket) => {
+					const { event } = packet;
+					profileReq.emit({ kinds: [0], authors: [event.pubkey], limit: 1 });
 
-          return [
-            {
-              kinds: [1],
-              search: search,
-              limit,
-              until: Math.floor(Date.now() / 1000),
-            },
-          ];
-        },
-      });
-    },
-    [addNotes]
-  );
+					addEvent(event);
+				});
 
-  useEffect(() => {
-    setNotes({});
-    if (query && app.mux) {
-      if (subscriptionRef.current) {
-        app.mux.unSubscribe(subscriptionRef.current);
-      }
-      subscriptionRef.current = subscribe(app.mux, query);
-    }
-  }, [app.mux, query, subscribe]);
+			const batchedReq = profileReq.pipe(bufferTime(10), batch());
 
-  const sortedNotes = Object.values(notes).sort(
-    (a: Event, b: Event) => b.created_at - a.created_at
-  );
+			app.rxNostr
+				.use(batchedReq)
+				.pipe(latestEach(({ event }) => event.pubkey))
+				.subscribe((packet: EventPacket) => {
+					const { event } = packet;
+					const profile = JSON.parse(event.content);
+					setProfiles((prev) => ({ ...prev, [event.pubkey]: profile }));
+				});
 
-  function handleMoreClick() {
-    setPossiblyMoreAvailable(false);
-    if (sortedNotes.length === 0) {
-      return;
-    }
-    let oldest = sortedNotes[sortedNotes.length - 1].created_at;
-    if (oldest === 0) {
-      oldest = 1; // There seems to be a relay that treats 0 as unspecified?
-    }
-    const subscription = app.mux?.subscribe({
-      filters: [{ kinds: [1], search: query, limit, until: oldest } as any],
-      onEvent: addNotes,
-      onEose() {
-        app.mux.unSubscribe(subscription);
-      },
-    });
-    setTimeout(() => {
-      app.mux.unSubscribe(subscription);
-    }, 10000);
-  }
+			rxReq.emit({ kinds: [1], search: query, limit });
+		}
 
-  return (
-    <div className="container mx-auto mt-5 px-2">
-      <QueryForm initialValue={query}></QueryForm>
+		return () => {
+			rxSubscriptionRef.current?.unsubscribe();
+		};
+	}, [app.rxNostr, query]);
 
-      <div className="mt-8">
-        {query && (
-          <div className="flex align-middle">
-            <div>
-              Search for <strong>{query}</strong> ({sortedNotes.length} hits)
-            </div>
-            <div className="relative flex h-3 w-3 ml-2 my-auto">
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
-            </div>
-          </div>
-        )}
-      </div>
+	const sortedNotes = Object.values(notes).sort(
+		(a: Event, b: Event) => b.created_at - a.created_at,
+	);
 
-      <div className="mt-2">
-        {sortedNotes.map((note: Event) => (
-          <Note note={note} key={note.id} />
-        ))}
-      </div>
-      {query && possiblyMoreAvailable && sortedNotes.length < hardLimit && (
-        <button className="my-5 link link-primary" onClick={handleMoreClick}>
-          More
-        </button>
-      )}
-    </div>
-  );
+	function handleMoreClick() {
+		// 	setPossiblyMoreAvailable(false);
+		// 	if (sortedNotes.length === 0) {
+		// 		return;
+		// 	}
+		// 	let oldest = sortedNotes[sortedNotes.length - 1].created_at;
+		// 	if (oldest === 0) {
+		// 		oldest = 1; // There seems to be a relay that treats 0 as unspecified?
+		// 	}
+		// 	const subscription = app.mux?.subscribe({
+		// 		filters: [{ kinds: [1], search: query, limit, until: oldest } as any],
+		// 		onEvent: addNotes,
+		// 		onEose() {
+		// 			app.mux.unSubscribe(subscription);
+		// 		},
+		// 	});
+		// 	setTimeout(() => {
+		// 		app.mux.unSubscribe(subscription);
+		// 	}, 10000);
+	}
+
+	return (
+		<div className="container mx-auto mt-5 px-2">
+			<QueryForm initialValue={query}></QueryForm>
+
+			<div className="mt-8">
+				{query && (
+					<div className="flex align-middle">
+						<div>
+							Search for <strong>{query}</strong> ({sortedNotes.length} hits)
+						</div>
+						<div className="relative flex h-3 w-3 ml-2 my-auto">
+							<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+						</div>
+					</div>
+				)}
+			</div>
+
+			<div className="mt-2">
+				{sortedNotes.map((note: Event) => (
+					<Note note={note} profile={profiles[note.pubkey]} key={note.id} />
+				))}
+			</div>
+			{query && possiblyMoreAvailable && sortedNotes.length < hardLimit && (
+				<button className="my-5 link link-primary" onClick={handleMoreClick}>
+					More
+				</button>
+			)}
+		</div>
+	);
 }
--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -1,65 +1,47 @@
 "use client";
 
-import {
-  AutoProfileSubscriber,
-  GenericProfile,
-  Mux,
-  parseGenericProfile,
-  Relay,
-} from "nostr-mux";
+import { createRxNostr, RxNostr } from "rx-nostr";
+import { verifier } from "rx-nostr-crypto";
+
 import { createContext, useContext, useEffect, useState } from "react";
 import { relays } from "./config";
 
 type AppContext = {
-  mux: Mux;
-  aps: AutoProfileSubscriber<GenericProfile>;
-  currentTime: Date;
+	rxNostr: RxNostr;
+	currentTime: Date;
 };
 
-const mux = new Mux();
-
-const aps = new AutoProfileSubscriber<GenericProfile>({
-  parser: parseGenericProfile,
-  autoEvict: false,
-  collectPubkeyFromEvent: (e, relayURL) => {
-    return relayURL && (e.kind === 1 || e.kind === 42) ? [e.pubkey] : [];
-  },
+const rxNostr = createRxNostr({
+	verifier,
+	connectionStrategy: "aggressive",
 });
-mux.installPlugin(aps);
+rxNostr.setDefaultRelays(relays);
 
 export const App = createContext<AppContext>({
-  mux,
-  aps,
-  currentTime: new Date(),
+	rxNostr,
+	currentTime: new Date(),
 });
 
 export function useApp() {
-  return useContext(App);
+	return useContext(App);
 }
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 10_000);
+	const [currentTime, setCurrentTime] = useState(new Date());
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCurrentTime(new Date());
+		}, 10_000);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
 
-  useEffect(() => {
-    for (const url of relays) {
-      mux.addRelay(new Relay(url));
-    }
-  }, []);
+	const ctx = {
+		rxNostr,
+		currentTime,
+	};
 
-  const ctx = {
-    mux,
-    aps,
-    currentTime,
-  };
-
-  return <App.Provider value={ctx}>{children}</App.Provider>;
+	return <App.Provider value={ctx}>{children}</App.Provider>;
 }
